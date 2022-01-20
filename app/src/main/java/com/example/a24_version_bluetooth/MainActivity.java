e622f3826e1c446989529e2deb23e131bb9cd5dc
deomi <deomit14@gmail.com>
2022-01-20 20:06:21 +0200

    20.01.2022

--- app/src/main/java/com/example/a24_version_bluetooth/MainActivity.java
+++ app/src/main/java/com/example/a24_version_bluetooth/MainActivity.java
@@ -3,15 +3,34 @@ package com.example.a24_version_bluetooth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_ENABLE_BT=0;
+    private static final int REQUEST_DISCOVERABLE_BT=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView out=(TextView)findViewById(R.id.out);
+        final Button button1 = (Button)findViewById(R.id.button1);
+        final Button button2 = (Button)findViewById(R.id.button2);
+        final Button button3 = (Button)findViewById(R.id.button3);
+        final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null){
+            out.append("device not supported");
+        }
     }
 
     public void sendMessage(View view){
