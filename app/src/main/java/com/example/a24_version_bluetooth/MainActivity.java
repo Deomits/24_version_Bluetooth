985034fa66ec80d428750512eacb0e397cbffe74
deomi <deomit14@gmail.com>
2022-01-21 08:50:38 +0200

    buttons and phone work

--- app/src/main/java/com/example/a24_version_bluetooth/MainActivity.java
+++ app/src/main/java/com/example/a24_version_bluetooth/MainActivity.java
@@ -16,9 +16,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_ENABLE_BT=0;
-    private static final int REQUEST_DISCOVERABLE_BT=0;
+public class MainActivity extends Activity {
+    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVERABLE_BT = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +31,45 @@ public class MainActivity extends AppCompatActivity {
         if(mBluetoothAdapter == null){
             out.append("device not supported");
         }
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if (!mBluetoothAdapter.isEnabled()){
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (!mBluetoothAdapter.isDiscovering()){
+                    out.append("MAKING YOUR DEVICE DISCOVERABLE");
+                    Toast.makeText(getApplicationContext(),"MAKING YOUR DEVICE DISCOVERABLE",
+                            Toast.LENGTH_LONG);
+
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_DISCOVERABLE_BT);
+                }
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0){
+                mBluetoothAdapter.disable();
+                //out.append("TURN_OFF BLUETOOTH");
+                Toast.makeText(getApplicationContext(), "TURNING_OFF BLUETOOTH", Toast.LENGTH_LONG);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
     }
 
     public void sendMessage(View view){
